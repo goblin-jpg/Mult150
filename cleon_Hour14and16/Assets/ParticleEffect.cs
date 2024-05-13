bfad5db2a880ae2706e5433b1e5051ac7036ee65
goblin-jpg <leonclaraj@gmail.com>
2024-05-12 22:38:29 -0700

    cleon_Hour14and16

---
+++ cleon_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    /*
+    Parameters modified:
+    1. Color changed to "Random between two colors": Light Green & Dark Green.
+    2. Start size changed to "Random between two constants": 1 & 5.
+    3. Playback speed changed to 0.5
+    4. Shape changed to "Donut".
+    5. Noise enabled, Strength changed to 2 & 10.
+
+
+
+
+
+    */
+}
